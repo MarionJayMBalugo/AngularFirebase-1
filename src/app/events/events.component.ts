781f3f27600a2ccc09b07a5f32dc8a8ba15fcfef
pathran-phantom1823 <patrick.pathran23@gmail.com>
2020-05-21 11:52:21 +0800

    adding angular

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Events } from "../Services/events";
 import { EventsService } from "../Services/events.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent implements OnInit {
+export class EventsComponent implements OnInit,OnDestroy {
   events:Events[];
   event:Events
   isShowList = true;
+  getEventSubscription:Subscription;
   constructor(
     private eventService:EventsService
   ) { }
 
   ngOnInit() {
-      this.events = this.eventService.getEvents()
+      this.getEventSubscription = this.eventService.getEvents().subscribe(res=>{
+          this.events = res
+      })
+  }
+  ngOnDestroy(){
+    this.getEventSubscription.unsubscribe();
   }
 
   alerts(){
@@ -24,11 +31,17 @@ export class EventsComponent implements OnInit {
   }
 
   getID(data:number){
-      this.event = this.eventService.getID(data)
-      this.isShowList = false
+    this.events.map(event=>{
+      if(event.id === data){
+        this.event = event
+        this.isShowList = false
+      }
+    })
+      // this.event = this.eventService.getID(data)
+      
   }
 
-  addEvent(event:any){
+  addEvent(event:Events){
     this.eventService.addEvent(event)
     console.log(event)
   }
